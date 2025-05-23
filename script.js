87234c1bbd04bdcf6231dc136ddbdce3309454da
Lixy_Chino <108574061+LixyChino@users.noreply.github.com>
2025-05-23 23:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function startCountdown() {
         }
     }, 1000);
 }
+
+const elem = document.documentElement;
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+    elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, Opera
+    elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+    elem.msRequestFullscreen();
+    }
+}
